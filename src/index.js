c3f9bc4ca5db46bef2f035deacfef5ed65dbac8a
Paulo-Teixeira <paulo.jtfs@:gmail.com>
2019-03-24 19:50:44 +0100

    WIP asset management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Shuttle from './shuttle.png';
 
 function component() {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Shuttle;
+
+  element.appendChild(myIcon);
 
   return element;
 }
